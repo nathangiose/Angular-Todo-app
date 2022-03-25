9e98e6eebda09f7dbeea028cd5d37754fce968e1
nathangiose <nathan@angleorange.com>
2022-03-25 05:07:06 +0200

    adding C of Crud

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
+  private todos;
+  private activeTasks;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
-  ngOnInit(): void {
+  getTodos(){
+    return this.todoService.get().then(todos => {
+      this.todos = todos;
+      this.activeTasks = this.todos.filter(todo => !todo.isDone).length;
+    });
   }
 
+  ngOnInit() {
+    this.getTodos();
+  }
 }
